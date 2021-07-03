eb331a20250fcff4ae623aff65a0614322c66776
bioerrorlog <n.iijima.0x01@gmail.com>
2021-07-03 23:07:54 +0900

    add: initialization in gen_2d

---
+++ cmd/gen.go
@@ -0,0 +1,71 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// genCmd represents the gen command
+var genCmd = &cobra.Command{
+	Use:   "gen",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(gen_2d())
+	},
+}
+
+func gen_2d() [][]int {
+	x := 5
+	y := 5
+	grid := [][]int{}
+	row := []int{}
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < y; i++ {
+		for j := 0; j < x; j++ {
+			row = append(row, rand.Intn(2))
+		}
+		grid = append(grid, row)
+		row = nil
+	}
+
+	return grid
+}
+
+func init() {
+	rootCmd.AddCommand(genCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// genCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// genCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
